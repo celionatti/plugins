dd75290da17528372846571c3335afb6755dec3b
celionatti <amisuusman@gmail.com>
2023-07-09 11:39:58 +0100

    Starting the project

---
+++ app/core/App.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core;
+
+class App
+{
+    public function index()
+    {
+        echo "Everything is working";
+    }
+}
--- index.php
+++ index.php
@@ -1,47 +1,27 @@
-<?php require("functions.php"); ?>
+<?php
 
-<?php 
-    $hero_titles = ['Home', 'Contact'];
+use Core\App;
 
-    $hero_titles = do_filter('hero_titles', $hero_titles);
-?>
+session_start();
 
-<!DOCTYPE html>
-<html lang="en">
+$minPHPVersion = "8.0";
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+if(phpversion() < $minPHPVersion)
+    die("You need a minimum PHP version of $minPHPVersion to run this app");
 
-<style>
-    body {
-        font-family: tahoma;
-    }
-</style>
 
-<body>
-    <main style="max-width: 1000px; margin:auto; padding:4px;">
-        <h1>This is a Plugins Website</h1>
-        <div style="display: flex;">
-            <div style="flex: 4; background:#eee; padding: 5px;">
-                <div style="padding: 10px;">
-                <?php foreach($hero_titles as $title): ?>
-                    <a href="#"><?= $title ?></a> |
-                <?php endforeach; ?>
-            </div>
-                <img src="images/image(4).jpg" style="max-width:100%;" alt="">
-            </div>
-            <div style="flex: 1; background:#ddd;">
-                <img src="images/image(1).jpg" style="max-width:100%;" alt="">
-                <img src="images/image(2).jpg" style="max-width:100%;" alt="">
-                <?php do_action('images_section'); ?>
-                <img src="images/image(3).jpg" style="max-width:100%;" alt="">
-                <img src="images/image(5).jpg" style="max-width:100%;" alt="">
-            </div>
-        </div>
-    </main>
-</body>
 
-</html>
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOTPATH', __DIR__. DIRECTORY_SEPARATOR);
+
+require 'config.php';
+require 'app'. DS . 'core' . DS . 'init.php';
+
+DEBUG ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
+
+$ACTIONS = [];
+$FILTERS = [];
+$APP['URL'] = split_url();
+
+$app = new \Core\App();
+$app->index();
